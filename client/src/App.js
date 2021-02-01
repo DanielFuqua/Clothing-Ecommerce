5580c4bd47780cd21a6078ef2cd6c874bdbec844
robdan89 <fuquardwork@gmail.com>
2021-02-01 11:25:16 -0600

    made directory, collection items and collection previews mobile responsive using media query

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route, Redirect } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
@@ -10,6 +9,7 @@ import { selectCurrentUser } from "./redux/user/user.selector";
 import { createStructuredSelector } from "reselect";
 import CheckoutPage from "./pages/checkout/checkout.component";
 import { checkUserSession } from "./redux/user/user.actions";
+import { GlobalStyle } from "./global.styles";
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -18,6 +18,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
